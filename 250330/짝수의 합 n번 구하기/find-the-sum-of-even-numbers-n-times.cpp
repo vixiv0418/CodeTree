70f08493c3b7d01fb959403487c446579bf1e8e9
vixiv0418 <kai2339@naver.com>
2025-03-30 22:19:31 +0900

    Title: 짝수의 합 n번 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250330/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int test;
+    cin >> test;
+    for(int i =0; i<3; i++) {
+        cin<<a<<b;
+        int sum=0;
+        for(j=a; j<=b; j++) {
+            if(j%2==0) {
+                sum += j;
+            }
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
