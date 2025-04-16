b70627cfadac497c2e804de84781be4e6c3ea8ee
vixiv0418 <kai2339@naver.com>
2025-04-16 11:18:37 +0900

    Title: 기수 정렬 구현, Time: 34ms, Memory: 1MB, Status: Passed - Codetree

--- 250416/기수 정렬 구현/implement-radix-sort.cpp
+++ 250416/기수 정렬 구현/implement-radix-sort.cpp
@@ -3,22 +3,22 @@
 using namespace std;
 
 int n;
-int arr[100000];
+int arr[100000]; //제한 조건 원소 값<= 100,000
 
 void Radix_Sort() {
-    int pos = 1;
-    for (int p = 0; p<6; p++) {
-        vector<int> arr_new[10];
+    int pos = 1; //digit을 구할 때, 자리수로 나눠야 하기 때문에 pos 선언
+    for (int p = 0; p<6; p++) { // 최대 자리수인 10만 즉, 6번째 자리수까지 나타내기 위해
+        vector<int> arr_new[10]; // 벡터 배열을 각 자리수에 대해 집어 넣을 때마다 초기화
         for(int i =0; i<n; i++) {
-            int digit = (arr[i] / pos) % 10;
-            arr_new[digit].push_back(arr[i]);
+            int digit = (arr[i] / pos) % 10; // digit 자리수의 값을 즉 벡터 배열의 인덱스 값에 맞춤
+            arr_new[digit].push_back(arr[i]); // 벡터 배열의 인덱스 값에 맞춰서 원소 값 넣음
         }
-        int index = 0;
-        for(int i =0; i<10; i++) {
-            for(int j =0; j<arr_new[i].size(); j++) 
-                arr[index++]=arr_new[i][j];
+        int index = 0; 
+        for(int i =0; i<10; i++) { // digit을 나타냄
+            for(int j =0; j<arr_new[i].size(); j++) //arr_new[i].size()는 각 digit 값에 들어있는 원소들의 개수
+                arr[index++]=arr_new[i][j]; // arr에 차례로 벡터 배열에 정렬된 값을 넣음
         }
-        pos *= 10;
+        pos *= 10; // 각 자리수를 구해야하므로 10씩 곱해줌
     }
 }
 
