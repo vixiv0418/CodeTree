1345be08e046fb80420045878ae145824f267464
vixiv0418 <kai2339@naver.com>
2025-04-18 09:19:32 +0900

    Title: 퀵 정렬 구현, Time: 36ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250418/퀵 정렬 구현/implement-quick-sort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#define MAX_N 100000
+using namespace std;
+
+int n;
+int arr[MAX_N];
+// 배열을 분할하고 피벗의 최종 위치를 반환
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];  // 피벗은 보통 마지막 원소로 선택
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);  // 피벗을 중간으로 이동
+    return i + 1;
+}
+
+// 퀵 정렬 함수 (재귀)
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pos = partition(arr, low, high);
+
+        quickSort(arr, low, pos - 1);   // 왼쪽 부분 정렬
+        quickSort(arr, pos + 1, high);  // 오른쪽 부분 정렬
+    }
+}
+
+
+
+int main() {
+    cin >> n;
+    
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    quickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
