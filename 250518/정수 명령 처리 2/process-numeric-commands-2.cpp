cf84332e487422bf6ce697bd01173891ae70c173
vixiv0418 <kai2339@naver.com>
2025-05-18 19:51:03 +0900

    Title: 정수 명령 처리 2, Time: 6ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 250518/정수 명령 처리 2/process-numeric-commands-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N;
+string command[10000];
+int A[10000];
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push") {
+            cin >> A[i];
+        }
+        if (command[i] == "push") {
+            cin >> A[i];
+        }
+
+
+
+
+
+
+
+
+    }
+
+    
+
+    return 0;
+}
