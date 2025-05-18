d83f227aa3a53fe4876e8ace4bafb6cd2693516e
vixiv0418 <kai2339@naver.com>
2025-05-18 19:57:04 +0900

    Title: 정수 명령 처리 2, Time: 18ms, Memory: 1MB, Status: Passed - Codetree

--- 250518/정수 명령 처리 2/process-numeric-commands-2.cpp
+++ 250518/정수 명령 처리 2/process-numeric-commands-2.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
+#include <queue>
 
 using namespace std;
 
 int N;
 string command[10000];
 int A[10000];
+queue<int> q;
 
 int main() {
     cin >> N;
@@ -14,21 +16,21 @@ int main() {
         cin >> command[i];
         if (command[i] == "push") {
             cin >> A[i];
+            q.push(A[i]);
         }
-        if (command[i] == "push") {
-            cin >> A[i];
+        if (command[i] == "front") {
+            cout << q.front() << endl;
         }
-
-
-
-
-
-
-
-
+        if (command[i] == "pop") {
+            cout << q.front() << endl;
+            q.pop();
+        }
+        if (command[i] == "size") {
+            cout << q.size() <<endl;
+        }
+        if (command[i] == "empty") {
+            cout << q.empty() <<endl;
+        }
     }
-
-    
-
     return 0;
 }
