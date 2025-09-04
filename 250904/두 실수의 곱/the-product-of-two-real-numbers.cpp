9c53a87d56ae92657f069c0dffddfb15490b03ef
vixiv <kai2339@naver.com>
2025-09-04 12:33:49 +0900

    Title: 두 실수의 곱, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250904/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5.26;
+    int b = 8.27;
+
+    cout<< fixed;
+    cout<<precision(3);
+
+    cout<<a<<endl<<b;
+    return 0;
+}
