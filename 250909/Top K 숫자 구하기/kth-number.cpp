283570b7f6ed759c85c63d4db986f55f8cbbf000
vixiv <kai2339@naver.com>
2025-09-09 15:14:09 +0900

    Title: Top K 숫자 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250909/Top K 숫자 구하기/kth-number.cpp
+++ 250909/Top K 숫자 구하기/kth-number.cpp
@@ -2,10 +2,10 @@
 #include <algorithm>
 using namespace std;
 
+int N,K;
+int arr[1000];
 
 int main() {
-    int N,K;
-    int arr[100];
     cin>>N>>K;
     for (int i=0; i<N; i++) {
         cin>>arr[i];
