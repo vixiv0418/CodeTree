9d734dbbf597703225db3357b7d0f69fc90c6adb
vixiv <kai2339@naver.com>
2025-09-09 15:05:21 +0900

    Title: Top K 숫자 구하기, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250909/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N,K;
+int arr[100];
+
+int main() {
+    cin>>N>>K;
+    for (int i=0; i<N; i++) {
+        cin>>arr[i];
+    }    
+    sort(arr,arr+N);
+    cout<<arr[K-1];
+    return 0;
+}
